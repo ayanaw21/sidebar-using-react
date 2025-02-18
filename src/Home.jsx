946a464908aa48c171487846a4f227b5c9f19175
Ayanaw Mengesha <ayanawmengesha21@gmail.com>
2025-02-17 20:55:24 -0800

    create components

---
+++ src/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useGlobalContext } from './context'
+
+const Home = () => {
+    // const {openSidebar,openModal} =useGlobalContext();
+    console.log(openSidebar)
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
